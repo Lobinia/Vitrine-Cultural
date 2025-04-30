2d2debc05593bf66e43c109daca3585dbad2930e
Maya Oliveira <mayajuris@gmail.com>
2025-04-30 16:20:20 -0400

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('form-artista').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Obrigado por se inscrever! Em breve entraremos em contato.');
+    this.reset();
+  });
+  
